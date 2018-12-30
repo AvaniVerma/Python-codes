0471c350ce0748e6c24dfa7c1bd0195fb5ea45aa
Avani Verma <avani.verma1998@gmail.com>
2018-12-30 15:08:46 +0530

    Take a break code

---
+++ Take a break.py
@@ -0,0 +1,7 @@
+import webbrowser
+import time
+
+
+for i in range(0,3,1):      #Run loop thrice
+    time.sleep(25*60)       # break after 25 minutes
+    webbrowser.open("https://www.youtube.com/watch?v=MtN1YnoL46Q")
